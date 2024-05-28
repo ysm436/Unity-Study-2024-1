bad07529677f757b87db1127d696c707361bea8b
requin0221 <landshark007@naver.com>
2024-05-29 01:57:55 +0900

    assignment 4-3

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -8,13 +8,16 @@ public class EnemyGenerator : MonoBehaviour
     public GameObject player;
     [Space]
     public GameObject enemy;
+    public GameObject item;
     public float SpawnCycle;
+    public int enemyPerItem;
     public Vector2 minSpawnPos;
     public Vector2 maxSpawnPos;
 
     Vector3 randomPos;
     Vector3 vectorToPlayer;
     float timer = 0;
+    int enemyCount = 0;
 
     private void Update()
     {
@@ -24,14 +27,26 @@ public class EnemyGenerator : MonoBehaviour
 
             do
             {
-            randomPos = new Vector3(Random.Range(minSpawnPos.x,maxSpawnPos.x), Random.Range(minSpawnPos.y,maxSpawnPos.y), 0);
-            vectorToPlayer = player.transform.position - randomPos;
+                randomPos = new Vector3(Random.Range(minSpawnPos.x,maxSpawnPos.x), Random.Range(minSpawnPos.y,maxSpawnPos.y), 0);
+                vectorToPlayer = player.transform.position - randomPos;
             }
             while(vectorToPlayer.magnitude < 1);
 
             Instantiate<GameObject>(enemy, randomPos, Quaternion.Euler(0,0,Mathf.Atan2(-vectorToPlayer.x,vectorToPlayer.y) * Mathf.Rad2Deg));
             timer = 0;
+            enemyCount++;
+            if (enemyCount >= enemyPerItem)
+            {
+                do
+                {
+                    randomPos = new Vector3(Random.Range(minSpawnPos.x,maxSpawnPos.x), Random.Range(minSpawnPos.y,maxSpawnPos.y), 0);
+                    vectorToPlayer = player.transform.position - randomPos;
+                }
+                while(vectorToPlayer.magnitude < 1);
+
+                Instantiate<GameObject>(item, randomPos, Quaternion.Euler(0,0,Mathf.Atan2(-vectorToPlayer.x,vectorToPlayer.y) * Mathf.Rad2Deg));
+                enemyCount = 0;
+            }
         }
-    
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,48 @@
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     public int speed;
+    int maxHp = 3;
+    int hp;
     Vector3 directionVector;
+    public GameObject bg_gameover;
+    public TextMeshProUGUI text_playerHP;
+
+    void Gameover()
+    {
+        Destroy(gameObject);
+        bg_gameover.SetActive(true);
+        GameObject.FindWithTag("EnemyGenerator").SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Enemy")
+        {
+            Destroy(c.gameObject);
+            hp--;
+            text_playerHP.text = hp.ToString() + "/" + maxHp.ToString();
+            if (hp == 0) Gameover();
+        }
+        if (c.gameObject.tag == "Item")
+        {
+            Destroy(c.gameObject);
+            if (hp < maxHp) { hp++; text_playerHP.text = hp.ToString() + "/" + maxHp.ToString(); }
+        }
+    }
+
+    private void Start()
+    {
+        hp = maxHp;
+        bg_gameover.SetActive(false);
+        text_playerHP.text = hp.ToString() + "/" + maxHp.ToString();
+    }
 
     private void Update()
     {
